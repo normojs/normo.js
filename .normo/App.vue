252fd3251975918e4b64d65159d8f2a058deec11
傅长路 <fulu0709@foxmail.com>
2021-03-03 14:35:47 +0800

    仿nuxtjs文件结构

--- .normo/App.vue
+++ .normo/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="px-4 py-10 text-center text-gray-700 dark:text-gray-200">
+    111
     <router-view />
     <Footer />
   </div>
--- normo.config.ts
+++ normo.config.ts
@@ -4,13 +4,32 @@ import { defineConfig } from 'vite'
 import Vue from '@vitejs/plugin-vue'
 import Pages from 'vite-plugin-pages'
 import ViteComponents from 'vite-plugin-components'
+// 布局
+import Layouts from 'vite-plugin-vue-layouts'
 
 export default defineConfig({
   // TODO https://github.com/vitejs/vite/issues/306
   // root: path.resolve(__dirname, '.normo'),
   alias: {
-    '@/': `${path.resolve(__dirname, 'src')}/`
+    '@/': `${path.resolve(__dirname, '')}/`
   },
+  publicDir: 'static',
+  plugins: [
+    // 支持vue
+    Vue(),
+    // 布局 https://github.com/JohnCampionJr/vite-plugin-vue-layouts
+    Layouts({
+      layoutsDir: 'layouts'
+    }),
+    // https://github.com/hannoeru/vite-plugin-pages
+    Pages({
+      pagesDir: 'pages'
+    }),
+    // https://github.com/antfu/vite-plugin-components
+    ViteComponents({
+      dirs: ['components']
+    })
+  ],
   build: {
     rollupOptions: {
       input: {
@@ -18,19 +37,7 @@ export default defineConfig({
         // nested: resolve(__dirname, 'nested/index.html')
       }
     }
-  },
-  plugins: [
-    // 支持vue
-    Vue(),
-    // https://github.com/hannoeru/vite-plugin-pages
-    Pages(),
-    // https://github.com/antfu/vite-plugin-components
-    ViteComponents({
-      // currently, vite does not provide an API for plugins to get the config https://github.com/vitejs/vite/issues/738
-      // as the `alias` changes the behavior of middlewares, you have to pass it to ViteComponents to do the resolving
-      // alias
-    })
-  ]
+  }
 })
 
 // export default config
